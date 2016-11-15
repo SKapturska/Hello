ca5e1d2828988e463fb03c53d9d80599f140b013
SKapturska <skapturska@us.edu.pl>
2016-11-15 09:57:20 +0100

    Koniec

--- src/hello/Window.java
+++ src/hello/Window.java
@@ -1,14 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hello;
 
-/**
- *
- * @author Sandi
- */
-public class Window {
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+public class Window extends JFrame implements ActionListener{
+
+    JLabel label;
+    JTextField textField;
+    JButton button;
+    String name;
+    
+    Window(){
+        setTitle("Hello");
+        setSize(350,200);
+        setLocation(300,200);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(null);
+        setResizable(false);
+        
+        label = new JLabel("Podaj imię ");
+        label.setSize(100,30);
+        label.setLocation(10,30);
+        label.setFont(new Font("SansSerif", Font.BOLD, 18));
+        add(label);
+        
+        textField = new JTextField();
+        textField.setSize(130,30);
+        textField.setLocation(120,30);
+        add(textField);
+        
+        button = new JButton("Click me!");
+        button.setSize(150,30);
+        button.setLocation(80,80);
+        add(button);
+        button.addActionListener(this);
+  
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        name = textField.getText().trim();
+        JOptionPane.showMessageDialog(null, "Cześć " + name, "Hi", JOptionPane.INFORMATION_MESSAGE);
+    }
     
 }
